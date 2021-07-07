125f814740f46211296dda8583548f839881b172
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-07 12:28:04 -0500

    feat:fib11

---
+++ IdeaProjects/TimsByte/src/main/java/com/CrushedMonkey/controllers/ViewControllers/stylesheet.java
@@ -0,0 +1,52 @@
+package com.CrushedMonkey.controllers.ViewControllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class stylesheet {
+
+    @GetMapping("/partials/css/stylesheet")
+    public String Welcome(){
+
+//        try{
+//            (
+//        }catch)
+        return "/partials/css/stylesheet";
+    }
+
+
+
+
+    // for ( int i = 1; i <= num ; i++)
+
+    /*
+
+
+
+      hello everyone
+
+
+      input x = o;
+      input y = 0;
+
+      pos = x || y;
+
+
+      for knight move x + 1 || y + 3
+
+
+
+
+
+
+
+
+
+     */
+    {
+        System.out.print("hello");
+    }
+
+}

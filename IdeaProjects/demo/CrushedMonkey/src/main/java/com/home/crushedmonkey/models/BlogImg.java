dd202f0e401613512119cf4ec3c547ec7056aea7
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-08-02 13:54:58 -0500

    feat: good working verison, added getters and setters:

--- IdeaProjects/demo/CrushedMonkey/src/main/java/com/home/crushedmonkey/models/BlogImg.java
+++ IdeaProjects/demo/CrushedMonkey/src/main/java/com/home/crushedmonkey/models/BlogImg.java
@@ -23,5 +23,47 @@ public class BlogImg {
     @JoinColumn (name = "BlogPost_id")
     private com.home.crushedmonkey.models.BlogPost BlogPost;
 
+  //insert
 
+    public BlogImg(String blogImg, com.home.crushedmonkey.models.BlogPost blogPost) {
+        BlogImg = blogImg;
+        BlogPost = blogPost;
+    }
+
+
+    //update
+
+    public BlogImg(long id, String blogImg, com.home.crushedmonkey.models.BlogPost blogPost) {
+        this.id = id;
+        BlogImg = blogImg;
+        BlogPost = blogPost;
+    }
+
+
+    //getters and setters
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBlogImg() {
+        return BlogImg;
+    }
+
+    public void setBlogImg(String blogImg) {
+        BlogImg = blogImg;
+    }
+
+    public com.home.crushedmonkey.models.BlogPost getBlogPost() {
+        return BlogPost;
+    }
+
+    public void setBlogPost(com.home.crushedmonkey.models.BlogPost blogPost) {
+        BlogPost = blogPost;
+    }
 }

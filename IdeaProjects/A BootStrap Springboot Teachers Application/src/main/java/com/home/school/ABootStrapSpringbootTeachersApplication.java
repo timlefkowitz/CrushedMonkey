bf421fe77a9a28c0a6c77dfdace3e535bf4c861b
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-08-02 23:06:48 -0500

    Feat: Teacher Application

---
+++ IdeaProjects/A BootStrap Springboot Teachers Application/src/main/java/com/home/school/ABootStrapSpringbootTeachersApplication.java
@@ -0,0 +1,13 @@
+package com.home.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ABootStrapSpringbootTeachersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ABootStrapSpringbootTeachersApplication.class, args);
+    }
+
+}
---
+++ IdeaProjects/A BootStrap Springboot Teachers Application/src/test/java/com/home/abootstrapspringbootteachersapplication/ABootStrapSpringbootTeachersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.abootstrapspringbootteachersapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ABootStrapSpringbootTeachersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

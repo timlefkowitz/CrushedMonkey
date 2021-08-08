4ca5ea9f7906e253faee4e9ea37a61ef2a6680ea
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-08-07 22:23:53 -0500

    feat: adding controllers for pages

--- IdeaProjects/demo/TimothyLefkowitz.com/src/main/java/com/photonlife/timothylefkowitzcom/Controllers/homeController.java
+++ IdeaProjects/demo/TimothyLefkowitz.com/src/main/java/com/photonlife/timothylefkowitzcom/Controllers/homeController.java
@@ -27,10 +27,80 @@ public class homeController {
         return "weathermap";
     }
 
-    @GetMapping("magazine")
+
+    @GetMapping("calc")
+    public String calc(Model model)
+    {
+        return "calc";
+    }
+
+    @GetMapping("3d")
+    public String threed(Model model)
+    {
+        return "3d";
+    }
+
+    @GetMapping("code")
+    public String code(Model model)
+    {
+        return "code";
+    }
+
+    @GetMapping("coffee")
+    public String coffee(Model model)
+    {
+        return "coffee";
+    }
+
+    @GetMapping("contact")
+    public String contact(Model model)
+    {
+        return "contact";
+    }
+
+    @GetMapping("contactsManager")
+    public String contactsManager(Model model)
+    {
+        return "contactsManager";
+    }
+
+    @GetMapping("favfoods")
+    public String favfoods(Model model)
+    {
+        return "favfoods";
+    }
+
+    @GetMapping("flickarchive")
+    public String flickArchive(Model model)
+    {
+        return "flickarchive";
+    }
+
+    @GetMapping("konami")
     public String magazine(Model model)
     {
-        return "magazine";
+        return "konami";
     }
+
+    @GetMapping("mag")
+    public String mag(Model model)
+    {
+        return "mag";
+    }
+
+    @GetMapping("min")
+    public String min(Model model)
+    {
+        return "min";
+    }
+
+    @GetMapping("NewIndex")
+    public String NewIndex(Model model)
+    {
+        return "NewIndex";
+    }
+
+
+
 
 }

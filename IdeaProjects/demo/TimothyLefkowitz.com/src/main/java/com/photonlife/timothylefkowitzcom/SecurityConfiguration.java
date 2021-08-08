7f04759337cbb73ab0d5627a7b74adf73cd9e2c2
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-08-07 22:34:04 -0500

    feat: adding spring security to timothy lefkwoitz.com

---
+++ IdeaProjects/demo/TimothyLefkowitz.com/src/main/java/com/photonlife/timothylefkowitzcom/SecurityConfiguration.java
@@ -0,0 +1,74 @@
+package com.photonlife.timothylefkowitzcom;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+public class SecurityConfiguration<UserDetailsLoader> extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsLoader usersLoader;
+
+    public SecurityConfiguration(UserDetailsLoader usersLoader) {
+        this.usersLoader = usersLoader;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(usersLoader) // How to find users by their username
+                .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                /* Login configuration */
+                .formLogin()
+
+                .loginPage("/index")
+                .defaultSuccessUrl("/home") // user's home page, it can be any URL
+
+                .permitAll() // Anyone can go to the login page
+                /* Logout configuration */
+                .and()
+                .logout()
+                .logoutSuccessUrl("/index?logout") // append a query string value
+                /* Pages that can be viewed without having to log in */
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/sign-up", "/login", "/index?logout") // anyone can see these pages
+                .permitAll()
+                /* Pages that require authentication */
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "post", // only authenticated users can create ads
+                        "/groupcreation", // only authenticated users can edit ads
+                        "groupcreation",
+                        "groups",
+                        "UserPostingForm",
+                        "UsersHome",
+                        "/UsersHome",
+                        "UsersProfile",
+                        "/post",
+                        "/{username}",
+                        "/groups"/// maybe anyone can see usersprofiles but not post
+
+                )
+                .authenticated()
+        ;
+    }
+}
